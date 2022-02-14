efa1526ce959a2fa4bdc701dde71891fa56493e3
codiy <mail@codiy.net>
2022-02-14 15:28:27 +0800

    Update installation script

--- README.md
+++ README.md
@@ -2,22 +2,25 @@
 
 ![](https://img.codiy.net/repo/dotfiles/2021-12/22220004.png)
 
-## How to Setup
+## How to setup
 
-* `curl -fsSL https://raw.githubusercontent.com/codiy1992/dotfiles/master/.scripts/setup.sh | bash`
-* or shorter `curl -fsSL https://git.io/JRK2L | bash`
+```
+curl -fsSL https://raw.githubusercontent.com/codiy1992/dotfiles/master/install.sh | bash
+```
 
-## How to Use
+## How to use
 
-* Use Command **`got`** Just like use **`git`**
+* Use command **`got`** just like use **`git`**
 * `got status`, `got diff`, `got pull`, `got push`
 
-## How to Make Yours
+## How to make yours
 
-* Create a Public Repository on Github.
-* Then Copy [the content of this file](https://raw.githubusercontent.com/codiy1992/dotfiles/master/.scripts/setup.sh)
-* Change Some Variables to Yours
-* Push this this file to Your Github Repository
-* set alias `alias got='git --git-dir=${HOME}/Bares/dotfiles.git --work-tree=${HOME}'` in Your `.zshrc` file
-* `source ~/.zshrc` And Then You can use `got` to Manage Your **dotfiles**.
+* Create a **public repository** for your own dotfiles on Github.
+* Then copy the content of [my installation script][1]
+* Change variables to yours
+* Commit and push it to your github repository
+* Create alias `alias got='git --git-dir=${HOME}/.dotfiles --work-tree=${HOME}'` in Your `.zshrc`  
+or `.bashrc` file and **source** it.
+* And then you can use `got` to manage your **dotfiles**.
 
+[1]: https://raw.githubusercontent.com/codiy1992/dotfiles/master/install.sh
---
+++ install.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# set -ex
+BARES_DIR=${HOME}
+REPOSITORY=${BARES_DIR}/.dotfiles
+REPO_DOMAIN=github.com
+REPO_NAME=codiy1992/dotfiles
+BACKUP_DIR=${HOME}/.dotfiles-backup
+
+cd ${HOME}
+
+if [ ! -d ${REPOSITORY} ] || [ "$(ls -A ${REPOSITORY})" = "" ]; then
+    # Clone Repository
+    mkdir -p ${BARES_DIR}
+    git clone --bare https://${REPO_DOMAIN}/${REPO_NAME}.git ${REPOSITORY}
+    # Backup old files
+    mkdir -p ${BACKUP_DIR}
+    git --git-dir=${REPOSITORY} --work-tree=${HOME} checkout 2>&1 \
+        | egrep '^[[:space:]]+.*' \
+        | awk '{print $1}' \
+        | xargs -I{} dirname {} \
+        | xargs -I{} mkdir -p ${BACKUP_DIR}/{}
+    git --git-dir=${REPOSITORY} --work-tree=${HOME} checkout 2>&1 \
+        | egrep '^[[:space:]]+.*' \
+        | awk '{print $1}' \
+        | xargs -I{} mv {} ${BACKUP_DIR}/{}
+    # Checkout Repository to your ${HOME}
+    git --git-dir=${REPOSITORY} --work-tree=${HOME} checkout
+    # Replace config
+    if [ "$(uname)" = "Darwin" ]; then
+        sed -i '' "s/^\turl = http.*/\turl = git@${REPO_DOMAIN}:${REPO_NAME//\//\\\/}.git/" ${REPOSITORY}/config
+    else
+        sed -i "s/^\turl = http.*/\turl = git@${REPO_DOMAIN}:${REPO_NAME//\//\\\/}.git/" ${REPOSITORY}/config
+    fi
+fi
+
+echo -e "\033[49;32mSuccessfully!
+Your \033[49;34mold dotfiles \033[49;32mhas been moved to \033[49;34m~/.dotfiles-backup\033[0m\033[49;32m.
+
+Now you need to add aliases showed below and \033[49;34msource your .zshrc or .bashrc file \033[49;32mmanually
+\033[49;34m
+alias got='git --git-dir=${HOME}/.dotfiles --work-tree=${HOME}'
+alias got.encrypt='${HOME}/secrets/encrypt'
+alias got.decrypt='${HOME}/secrets/decrypt'
+\033[49;32m
+
+and then you can use \033[49;34m\`got\`\033[49;32m command to manage your dotfiles.\033[0m"
